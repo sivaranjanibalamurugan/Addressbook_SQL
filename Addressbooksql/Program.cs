17f69b06eb60d700b4d389cb98256013e9af26d8
unknown <sivaranjanib99@gmail.com>
2021-08-04 23:15:19 +0530

    [siva] welcome to address book services

---
+++ Addressbooksql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Addressbooksql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to address book db");
+        }
+    }
+}
